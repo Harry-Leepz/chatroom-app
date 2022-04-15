832e082f98e6608f3ecd588a7ad2c5a253e8a266
Harry <hdhillon478@gmail.com>
2022-04-15 15:32:53 +0100

    Add addChat method to add documents to firebase

--- assets/scripts/chat.js
+++ assets/scripts/chat.js
@@ -4,6 +4,30 @@ class Chatroom {
     this.username = username;
     this.chats = db.collection("chats");
   }
+
+  async addChat(message) {
+    // format a chat object
+    const now = new Date();
+    const chat = {
+      message: message,
+      username: this.username,
+      room: this.room,
+      created_at: firebase.firestore.Timestamp.fromDate(now),
+    };
+
+    // save chat document
+    const response = await this.chats.add(chat);
+    return response;
+  }
 }
 
 const chatroom = new Chatroom("gaming", "harry");
+
+chatroom
+  .addChat("hello everyone")
+  .then(() => {
+    console.log("chat added");
+  })
+  .catch((error) => {
+    console.log(error);
+  });
