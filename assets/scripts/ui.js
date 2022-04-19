a3abedff52f81817a97dadf4915a9582867e4016
Harry <hdhillon478@gmail.com>
2022-04-19 12:05:50 +0100

    Update styling and add new date format

--- assets/scripts/ui.js
+++ assets/scripts/ui.js
@@ -4,11 +4,14 @@ class ChatUI {
   }
 
   render(data) {
+    const when = dateFns.distanceInWordsToNow(data.created_at.toDate(), {
+      addSuffix: true,
+    });
     const html = `
       <li class="list-group-item">
         <span class="username">${data.username} says...</span>
         <span class="message">${data.message}</span>
-        <div class="message">${data.created_at.toDate()}</div>
+        <div class="time">${when}</div>
       </li>
     `;
 
