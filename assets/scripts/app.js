20a7b05feb277080b521ea33b35cc34d38565eb7
Harry <hdhillon478@gmail.com>
2022-04-19 11:06:41 +0100

    Add new ChatUI class in chat.js

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,5 +1,11 @@
+// dom queries
+const chatList = document.querySelector(".chat-list");
+
+// new chat instances
+const chatUI = new ChatUI(list);
 const chatroom = new Chatroom("general", "harry");
 
+// get chats and render templates
 chatroom.getChats((data) => {
   console.log(data);
 });
